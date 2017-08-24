2b0a77a4fe6f3021518bad55af355fee60be71ec
Will Ferrer <wferrer@xfire.com>
2017-08-23 20:12:05 -0700

    Fixed acl middleware

--- src/Constants/PermissionsTemplatesConstants.php
+++ src/Constants/PermissionsTemplatesConstants.php
@@ -11,10 +11,10 @@ namespace TempestTools\AclMiddleware\Constants;
 use TempestTools\Common\Laravel\Utility\Extractor;
 
 class PermissionsTemplatesConstants{
-    const URI = '?{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:uri}}';
-    const URI_AND_REQUEST_METHOD = '?{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:uri}}:{{:' . Extractor::EXTRACTOR_KEY_NAME . ':request:method}}';
-    const ACTION_NAME = '?{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:actionName}}';
-    const ACTION_NAME_AND_REQUEST_METHOD = '?{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:actionName}}:{{:' . Extractor::EXTRACTOR_KEY_NAME . ':request:method}}';
-    const DOMAIN = '?{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:domain}}';
-    const ENVIRONMENT = '?{{:' . Extractor::EXTRACTOR_KEY_NAME . ':environment}}';
+    const URI = '{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:uri}}';
+    const URI_AND_REQUEST_METHOD = '{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:uri}}:{{:' . Extractor::EXTRACTOR_KEY_NAME . ':request:method}}';
+    const ACTION_NAME = '{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:actionName}}';
+    const ACTION_NAME_AND_REQUEST_METHOD = '{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:actionName}}:{{:' . Extractor::EXTRACTOR_KEY_NAME . ':request:method}}';
+    const DOMAIN = '{{:' . Extractor::EXTRACTOR_KEY_NAME . ':route:domain}}';
+    const ENVIRONMENT = '{{:' . Extractor::EXTRACTOR_KEY_NAME . ':environment}}';
 }
