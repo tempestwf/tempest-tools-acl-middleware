23d8ab05dd65c10668b3c3d5d0d523e672a5477e
Will Ferrer <wferrer@xfire.com>
2017-10-24 16:46:44 -0700

    Basic get working for albums

--- src/Constants/PermissionsTemplatesConstants.php
+++ src/Constants/PermissionsTemplatesConstants.php
@@ -12,6 +12,7 @@ use TempestTools\Common\Constants\CommonArrayObjectKeyConstants;
 
 class PermissionsTemplatesConstants{
     const URI = '{{:' . CommonArrayObjectKeyConstants::FRAMEWORK_KEY_NAME . ':route:uri}}';
+    const URI_FORCE_FIRST_SLASH = '/{{:' . CommonArrayObjectKeyConstants::FRAMEWORK_KEY_NAME . ':route:uri}}';
     const URI_AND_REQUEST_METHOD = '{{:' . CommonArrayObjectKeyConstants::FRAMEWORK_KEY_NAME . ':route:uri}}:{{:' . CommonArrayObjectKeyConstants::FRAMEWORK_KEY_NAME . ':request:method}}';
     const ACTION_NAME = '{{:' . CommonArrayObjectKeyConstants::FRAMEWORK_KEY_NAME . ':route:actionName}}';
     const ACTION_NAME_AND_REQUEST_METHOD = '{{:' . CommonArrayObjectKeyConstants::FRAMEWORK_KEY_NAME . ':route:actionName}}:{{:' . CommonArrayObjectKeyConstants::FRAMEWORK_KEY_NAME . ':request:method}}';
